d4b30689094b5bfe3b4bcde70f4f9f7f49eae3b2
bussiere <bussiere@gmail.com>
2022-01-18 10:32:01 +0100

    wip main Updated: 2022-01-18 10:32:00

--- readkif.py
+++ readkif.py
@@ -2,21 +2,21 @@ import shogi.KIF
 import shogi
 import shutil
 import time
-  
-# ts stores the time in seconds
-ts = str(time.time()).replace('.',"")
 
-kif=shogi.KIF.Parser.parse_file('partie_01.kif')[0]
+# ts stores the time in seconds
+ts = str(time.time()).replace(".", "")
+
+kif = shogi.KIF.Parser.parse_file("partie_01.kif")[0]
 board = shogi.Board(kif["sfen"])
-shutil.copy('partie_01.kif', 'old/'+ts+'_partie_01.kif')
+shutil.copy("partie_01.kif", "old/" + ts + "_partie_01.kif")
 print(board)
 print("\n")
 print(shogi.Move(shogi.B2, shogi.B3).usi())
 print("\n")
-print(shogi.Move.from_usi('8g8f') in board.legal_moves)
+print(shogi.Move.from_usi("8g8f") in board.legal_moves)
 print("\n")
 print(board.legal_moves)
-board.push_usi('8g8f')
+board.push_usi("8g8f")
 print(board)
 print("\n")
 print(board.kif_str())
